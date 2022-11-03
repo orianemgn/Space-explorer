8aac443ec29ccdd0a8d8d943404658c036517e91
orianemgn <mgnoriane@gmail.com>
2022-11-03 22:19:35 +0100

    test design image of the days

--- src/components/planets/ApodContainer.jsx
+++ src/components/planets/ApodContainer.jsx
@@ -3,6 +3,7 @@ const ApodContainer = (props) => {
         <>
                 <div className="apod-card">
                     <h1> Image of the Day </h1>
+                    <img className="image-mobile" src={props.apod.url} alt={props.apod.title} />
                     <h3>Copyright to {props.apod.copyright}</h3>
                     <p>{props.apod.explanation}</p>
                 </div>          
