2946ca421a60b5d24393fa809a2e9aaf0dc840d0
orianemgn <mgnoriane@gmail.com>
2022-11-09 11:41:07 +0100

    pictures for planets cards and data

--- src/planetsitemsList.js
+++ src/planetsitemsList.js
@@ -1,34 +1,42 @@
 export const planetsitems = [
     {
         title: 'Uranus',
-        img: ''
+        img: '',
+        distanceFromEarth: '2,723,950,000 km'
     },
     {
         title: 'Neptune',
-        img: ''
+        img: '',
+        distanceFromEarth: '4,351,400,000 km'
     },
     {
         title: 'Jupiter',
-        img: ''
+        img: '',
+        distanceFromEarth: '628,730,000 km'
     },
     {
         title: 'Mars',
-        img: ''
+        img: '',
+        distanceFromEarth: '78,340,000 km'
     },
     {
         title: 'Mercury',
-        img: ''
+        img: '',
+        distanceFromEarth: 'xxx km'
     },
     {
         title: 'Saturn',
-        img: ''
+        img: '',
+        distanceFromEarth: '1,275,000,000 km'
     }, 
     {
         title: 'Earth',
-        img: ''
+        img: '',
+        distanceFromEarth: '0 km'
     },
     {
         title: 'Venus',
-        img: ''
+        img: '',
+        distanceFromEarth: 'xxx km'
     }
 ]
