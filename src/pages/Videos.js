53b54063b6249cc3fa838899f924b435ea3227a4
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-10 17:22:43 +0100

    Delete Video-Webpage and work on HomePage

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,14 +1,14 @@
-import React from "react";
-import BackgroundVideo from "../components/videos/BackgroundVideo";
-import VideoCarousel from "../components/videos/VideoCarousel";
+// import React from "react";
+// import BackgroundVideo from "../components/videos/BackgroundVideo";
+// import VideoCarousel from "../components/videos/VideoCarousel";
 
-function Videos() {
-  return (
-    <div>
-      <VideoCarousel />
-      <BackgroundVideo />
+// function Videos() {
+//   return (
+//     <div>
+//       <VideoCarousel />
+//       <BackgroundVideo />
       
-    </div>
-  );
-}
-export default Videos;
+//     </div>
+//   );
+// }
+// export default Videos;
