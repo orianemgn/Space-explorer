6713de85ee56f5bd41514cb215d95620471aa0c3
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-16 11:13:13 +0100

    fixing infinite loop

--- src/components/video/VideoPage.jsx
+++ src/components/video/VideoPage.jsx
@@ -1,34 +1,51 @@
 import React from "react";
 import "./VideoPage.css";
+import { useEffect } from "react";
 import ReactPlayer from "react-player";
 
 
-const parallax = document.getElementById("parallax");
+// const parallax = document.getElementById("parallax");
 // Parallax Effect for DIV 1
-window.addEventListener("scroll", function () {
-  let offset = window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+// window.addEventListener("scroll", function () {
+//   let offset = window.pageYOffset;
+//   parallax.style.backgroundPositionY = offset * 0.6 + "px";
 
 
-  // DIV 1 background will move slower than other elements on scroll.
-}); 
+
+
+//   // DIV 1 background will move slower than other elements on scroll.
+// }); 
 
 export default function VideoPage() {
+  const parallax = document.getElementById("parallax");
+  useEffect(() => {
+    function handleParallax () {
+      let offset = window.pageYOffset;
+      parallax.style.backgroundPositionY = offset * 0.6 + 'px';
+      // DIV 1 background will move slower than other elements on scroll.
+      
+    }
+    window.addEventListener("scroll",handleParallax); 
+  
+    return () => {
+      window.removeEventListener("scroll",handleParallax);
+    }
+  }, [])
   return (
     <div className="body-videopage">
       <>
         <section>
-          <div id="parallax" class="parallax-item">
+          <div id="parallax" className="parallax-item">
             <h2>Div 1</h2>
           </div>
           <div class="react-player">
             <ReactPlayer width={'60%'} controls url="https://www.youtube.com/watch?v=86YLFOog4GM" />
             
           </div>
-          <div class="parallax-item">
+          <div className="parallax-item">
             <h2>Div 3</h2>
           </div>
-          <div class="parallax-item">
+          <div className="parallax-item">
             <h2>Div 4</h2>
           </div>
         </section>
