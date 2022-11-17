2689a866da8b92ae25a90618121ef7134ef99339
orianemgn <mgnoriane@gmail.com>
2022-11-17 16:06:35 +0100

    changes design planets

--- src/components/planets/planets-glossary/PlanetCard.js
+++ src/components/planets/planets-glossary/PlanetCard.js
@@ -1,12 +1,19 @@
 const PlanetCard = (props) => {
-  const { englishName, isPlanet, density, gravity, distanceFromEarth } =
-    props.data;
+  const {
+    englishName,
+    isPlanet,
+    density,
+    gravity,
+    distanceFromEarth,
+    description,
+  } = props.data;
 
   return (
     <div className="planet-card">
       <div>
         <h2>{englishName}</h2>
         <p>{isPlanet ? "is planet" : "is no planet"}</p>
+        <p>{description}</p>
         <p>Density: {density}</p>
         <p>Gravity: {gravity}</p>
         <p>Distance from Earth {distanceFromEarth}</p>
--- src/components/planets/planets-glossary/planetsItemsList.js
+++ src/components/planets/planets-glossary/planetsItemsList.js
@@ -12,40 +12,49 @@ export const planetsItemsList = [
     title: "Uranus",
     img: Uranus,
     distanceFromEarth: "2,723,950,000 km",
+    description: "",
   },
   {
     title: "Neptune",
     img: Neptune,
     distanceFromEarth: "4,351,400,000 km",
+    description:
+      "Neptune is the eighth planet from the sun in our solar system. This blue gas giant is far larger than Earth, at more than 17 times Earth's mass and nearly 58 times Earth's volume, according to NASA (opens in new tab). Neptune's rocky core is surrounded by a slushy fluid mix of water, ammonia and methane ice.",
   },
   {
     title: "Jupiter",
     img: Jupiter,
     distanceFromEarth: "628,730,000 km",
+    description: "",
   },
   {
     title: "Mars",
     img: Mars,
     distanceFromEarth: "78,340,000 km",
+    description: "",
   },
   {
     title: "Mercury",
     img: Mercury,
     distanceFromEarth: "xxx km",
+    description: "",
   },
   {
     title: "Saturn",
     img: Saturn,
     distanceFromEarth: "1,275,000,000 km",
+    description: "",
   },
   {
     title: "Earth",
     img: Earth,
     distanceFromEarth: "0 km",
+    description: "",
   },
   {
     title: "Venus",
     img: Venus,
     distanceFromEarth: "xxx km",
+    description: "",
   },
 ];
