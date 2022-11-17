5713b9285cb4b4f471b5cbaaa17fb82d7abc1e8a
stefan <stefan@stefan.com>
2022-11-17 09:54:54 +0100

    tweaked animations

--- src/components/news-card/NewsCard.jsx
+++ src/components/news-card/NewsCard.jsx
@@ -5,14 +5,15 @@ import styles from "./styles.module.css";
 function NewsCard({ title, source, url }) {
   return (
     <motion.div
-      whileInView={{ y: [100, 0], opacity: [0, 1] }}
+      whileInView={{ x: [100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.8, ease: "easeIn" }}
+      viewport={{ once: true }}
       className={styles.newsCard}>
       <h2>{title}</h2>
       <p>Source: {source}</p>
 
       <a href={url} target="_blank" rel="noreferrer">
-        <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
+        <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           Read More
         </motion.button>
       </a>
