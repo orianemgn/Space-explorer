2906acec093a43692009cbbcd79a9b9362ef0d77
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-17 10:56:52 +0100

    add style on div4

--- src/components/video/VideoPage.jsx
+++ src/components/video/VideoPage.jsx
@@ -46,8 +46,20 @@ export default function VideoPage() {
                 controls
                 url="https://www.youtube.com/watch?v=86YLFOog4GM"
               />
-              
               <h2>Nasa LiveStream</h2>
+              </motion.div>
+              <motion.div
+              whileInView={{ x: [-800, 50], opacity: [0, 1] }}
+              transition={{ duration: 1.2, ease: "easeIn" }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              viewport={{ once: true }}
+              className="pharagraph1">
+              <p>THIS WILL SHOW LIVE and  PRE-RECORDED FOOTAGE - depending on signal from the station or if the ISS is on the night side of Earth.
+
+When the feed is live the words LIVE NOW will appear in the top left hand corner of the screen.
+As the Space Station passes into a period of night every 45 mins video is unavailable - during this time, and other breaks in transmission,  recorded footage is shown .
+When back in daylight the live stream of earth will recommence</p>
               </motion.div>
           </div>
           <div className="parallax-item">
@@ -55,14 +67,16 @@ export default function VideoPage() {
             <p></p>
           </div>
           <div className="parallax-item">
-            <div className="react-player2">
+            <motion.div
+            whileInView={{ x: [50, 800], opacity: [0, 1] }}
+            transition={{ duration: 1.2, ease: "easeIn" }} className="react-player2">
               <ReactPlayer
                 width={""}
                 controls
                 url="https://https://www.youtube.com/watch?v=gosjiD288Jk&t=1252s"
               />
               <h2>The awesomeness of space</h2>
-            </div>
+            </motion.div>
           </div>
         </section>
       </>
