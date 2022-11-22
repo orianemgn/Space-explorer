3c0a8b66dd32d123d1ee821c546987c21f426114
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-22 12:30:31 +0100

    last commits

--- src/components/video/VideoPage.jsx
+++ src/components/video/VideoPage.jsx
@@ -4,24 +4,13 @@ import { useEffect, useRef } from "react";
 import ReactPlayer from "react-player";
 import { motion } from "framer-motion";
 
-// const parallax = document.getElementById("parallax");
-// Parallax Effect for DIV 1
-// window.addEventListener("scroll", function () {
-//   let offset = window.pageYOffset;
-//   parallax.style.backgroundPositionY = offset * 0.6 + "px";
-
-//   // DIV 1 background will move slower than other elements on scroll.
-// });
-
 export default function VideoPage() {
   const paralax2 = useRef();
 
-  // const parallax = document.getElementById("parallax");
   useEffect(() => {
     function handleParallax() {
       let offset = window.pageYOffset;
       paralax2.current.style.backgroundPositionY = offset * 0.6 + "px";
-      // DIV 1 background will move slower than other elements on scroll.
     }
     window.addEventListener("scroll", handleParallax);
 
@@ -34,13 +23,15 @@ export default function VideoPage() {
       <>
         <section>
           <div ref={paralax2} id="parallax" className="parallax-item">
-            <h2>Space in Motion</h2>
+            <h3 className="video-title">Space in Motion</h3>
           </div>
           <div id="parallax" className="parallax-item">
             <motion.div
               whileInView={{ y: [100, 0], opacity: [0, 1] }}
               transition={{ duration: 1, ease: "easeIn" }}
-              className="react-player">
+              viewport={{ once: true }}
+              className="react-player"
+            >
               <ReactPlayer
                 width={""}
                 controls
@@ -54,7 +45,8 @@ export default function VideoPage() {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
               viewport={{ once: true }}
-              className="pharagraph1">
+              className="pharagraph1"
+            >
               <p>
                 THIS WILL SHOW LIVE and PRE-RECORDED FOOTAGE - depending on
                 signal from the station or if the ISS is on the night side of
@@ -68,14 +60,17 @@ export default function VideoPage() {
             </motion.div>
           </div>
           <div className="parallax-item">
-            <h2>Div 3</h2>
+            <h2>Welcome to the Moon</h2>
             <p></p>
           </div>
+
           <div className="parallax-item">
             <motion.div
-              whileInView={{ x: [50, 800], opacity: [0, 1] }}
-              transition={{ duration: 1.2, ease: "easeIn" }}
-              className="react-player2">
+              whileInView={{ x: [-100, 30], opacity: [0, 1] }}
+              transition={{ duration: 1.2, ease: "easeOut" }}
+              viewport={{ once: true }}
+              className="react-player2"
+            >
               <ReactPlayer
                 width={""}
                 controls
@@ -86,16 +81,22 @@ export default function VideoPage() {
             <motion.div
               whileInView={{ y: [-500, 0], opacity: [0, 1] }}
               transition={{ duration: 1.2, ease: "easeIn" }}
-              className="pharagraph2">
+              viewport={{ once: true }}
+              className="pharagraph2"
+            >
               <p>
                 THIS WILL SHOW LIVE and PRE-RECORDED FOOTAGE - depending on
                 signal from the station or if the ISS is on the night side of
                 Earth. When the feed is live the words LIVE NOW will appear in
-                the top left hand corner of the screen. As the Space Station
-                passes into a period of night every 45 mins video is unavailable
-                - during this time, and other breaks in transmission, recorded
-                footage is shown . When back in daylight the live stream of
-                earth will recommence
+                the top left hand corner of the screen.
+              </p>
+
+              <p>
+                {" "}
+                As the Space Station passes into a period of night every 45 mins
+                video is unavailable - during this time, and other breaks in
+                transmission, recorded footage is shown . When back in daylight
+                the live stream of earth will recommence
               </p>
             </motion.div>
           </div>
