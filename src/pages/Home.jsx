e6ee97625aaebb6988c77f8a02888f3d6a760b08
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-23 11:05:30 +0100

    try useEffect on HomeVideo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useRef, useEffect} from "react";
 import videoBg from "../assests/Vid1.mp4";
 import "./home.css";
 import { motion } from "framer-motion";
 
 const Home = () => {
+  const video = useRef(null)
+  useEffect(() => {
+    console.log('fired')
+    video.current.play()
+    .then(msg => console.log('msg from play' + msg + video.current))
+    .catch(err => console.log('error from play' + err))
+  })
+  
+  console.log(video.current)
   return (
     <div className="main">
-      <video src={videoBg} type="video/mp4" playsinline autoPlay loop muted />
+      <video ref={video} src={videoBg} type="video/mp4" playsInline autoPlay loop muted />
       <motion.div 
       whileInView={{ y: [100, 20], opacity: [0, 1] }}
       transition={{ duration: 0.8, ease: "easeIn" }}
